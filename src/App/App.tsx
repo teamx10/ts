1b3c964dce94a020bb829c301ff3536b4970ce69
Artem Koziar <koziar.artem@gmail.com>
2021-05-10 13:05:34 +0300

    Lesson1 State

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Button, makeStyles, Paper, TextField, Typography} from "@material-ui/core";
 
@@ -7,6 +7,11 @@ const useStyles = makeStyles((theme) => ({
     width: 400,
     margin: '20px auto',
     justifyContent: 'center',
+  },
+  PaperAll: {
+    width: 400,
+    margin: '20px auto',
+    textAlign: 'left'
   },
   Item: {
     margin: 20,
@@ -19,28 +24,38 @@ const useStyles = makeStyles((theme) => ({
 function App() {
   const classes = useStyles();
 
+  const [a, setA] = useState('a');
+  const [b, setB] = useState('b');
+  const [c, setC] = useState('c');
+  const [d, setD] = useState('d');
+
+  const [all, setAll] = useState({ a, b, c, d });
+
   return (
     <div className="App">
-      <form noValidate autoComplete="off">
+      <form noValidate autoComplete="off" onSubmit={(e) => {e.preventDefault();  setAll({a, b, c, d}); }}>
         <Paper className={classes.Paper}>
           <Typography variant="h4">TS Form Example</Typography>
           <div className={classes.Item}>
-            <TextField label="A" fullWidth />
+            <TextField label="A" fullWidth value={a} onChange={(e) => setA(e.target.value)} />
           </div>
           <div className={classes.Item}>
-            <TextField label="B" fullWidth />
+            <TextField label="B" fullWidth value={b} onChange={(e) => setB(e.target.value)} />
           </div>
           <div className={classes.Item}>
-            <TextField label="C" fullWidth />
+            <TextField label="C" fullWidth value={c} onChange={(e) => setC(e.target.value)} />
           </div>
           <div className={classes.Item}>
-            <TextField label="D" fullWidth />
+            <TextField label="D" fullWidth value={d} onChange={(e) => setD(e.target.value)} />
           </div>
           <div className={classes.Item}>
-            <Button variant="outlined" className={classes.Button}>Default</Button>
+            <Button variant="outlined" className={classes.Button} type="submit">Submit</Button>
           </div>
         </Paper>
       </form>
+      <Paper className={classes.PaperAll}>
+        <pre>{JSON.stringify(all, null, 2)}</pre>
+      </Paper>
     </div>
   );
 }
