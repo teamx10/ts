87490e5b1b0f181a8c4d3476f7921961b0eb2d30
Artem Koziar <koziar.artem@gmail.com>
2021-05-10 12:07:57 +0300

    Form Material UI

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,12 +1,45 @@
 import React from 'react';
 import './App.css';
-import {TextField} from "@material-ui/core";
+import {Button, makeStyles, Paper, TextField, Typography} from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  Paper: {
+    width: 400,
+    margin: '20px auto',
+    justifyContent: 'center',
+  },
+  Item: {
+    margin: 20,
+  },
+  Button: {
+    marginBottom: 20
+  }
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
     <div className="App">
       <form noValidate autoComplete="off">
-        <TextField id="standard-basic" label="Standard" />
+        <Paper className={classes.Paper}>
+          <Typography variant="h4">TS Form Example</Typography>
+          <div className={classes.Item}>
+            <TextField label="A" fullWidth />
+          </div>
+          <div className={classes.Item}>
+            <TextField label="B" fullWidth />
+          </div>
+          <div className={classes.Item}>
+            <TextField label="C" fullWidth />
+          </div>
+          <div className={classes.Item}>
+            <TextField label="D" fullWidth />
+          </div>
+          <div className={classes.Item}>
+            <Button variant="outlined" className={classes.Button}>Default</Button>
+          </div>
+        </Paper>
       </form>
     </div>
   );
